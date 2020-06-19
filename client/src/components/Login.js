e91c373e032be190ad047e3e795e987852191a56
Wayne <wabbruscatoorr@gmail.com>
2020-06-19 12:47:46 -0400

    updating PrivateRoute

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-const initialState = {
-  credentials: {
-    username: "",
-    password: ""
-  }
-};
-
-const Login = (state = initialState, action) => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  axiosWithAuth()
-    .post("/api/colors", this.state.credentials)
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  });
 
   const handleChange = () => {
-    setState({
-      username: this.state.username,
-      password: this.state.password
+    return setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
     });
+  };
+
+  const onSubmit = e => {
+    e.prevenDefault();
+    axios
+      .post("http://localhost:5000/api/login", credentials)
+      .then(res => {
+        //res.data.payload
+        window.localStorage.setItem("token", res.data.payload);
+        props.history.push("bubbles");
+      })
+      .catch(err => console.log(err));
   };
 
   return (
@@ -27,15 +33,27 @@ const Login = (state = initialState, action) => {
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
       <from onSubmit={onSubmit}>
-        <label htmlFor="name">
+        <label>
+          Name:
           <input
             id="name"
             name="name"
             type="text"
             onChange={handleChange}
-            value={this.state.username}
+            value={credentials.username}
           />
         </label>
+        <label>
+          Password:
+          <input
+            id="password"
+            name="password"
+            type="text"
+            onChange={handleChange}
+            value={credentials.password}
+          />
+        </label>
+        <button>Submit</button>
       </from>
     </>
   );
